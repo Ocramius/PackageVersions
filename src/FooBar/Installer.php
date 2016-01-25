c28124d5697e02b3920ef85518352364c1d9fe39
Marco Pivetta <ocramius@gmail.com>
2016-01-25 17:16:49 +0100

    HAHA

--- src/FooBar/Installer.php
+++ src/FooBar/Installer.php
@@ -2,7 +2,17 @@
 
 namespace FooBar;
 
-class Installer
-{
+use Composer\Composer;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
 
+class Installer implements PluginInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function activate(Composer $composer, IOInterface $io)
+    {
+        $io->write('HAHA');
+    }
 }
