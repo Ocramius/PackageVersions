d2fccb19d4f37a4ffdafc7a90925c83fc5ddd6f7
Marco Pivetta <ocramius@gmail.com>
2016-01-25 21:58:21 +0100

    Reworking installer test to test all other scenarios as well

--- test/PackageVersionsTest/InstallerTest.php
+++ test/PackageVersionsTest/InstallerTest.php
@@ -13,18 +13,45 @@ use PHPUnit_Framework_TestCase;
  */
 final class InstallerTest extends PHPUnit_Framework_TestCase
 {
+    /**
+     * @var Composer|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $composer;
+
+    /**
+     * @var EventDispatcher|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $eventDispatcher;
+
+    /**
+     * @var IOInterface|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $io;
+
+    /**
+     * @var Installer
+     */
+    private $installer;
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->installer       = new Installer();
+        $this->io              = $this->getMock(IOInterface::class);
+        $this->composer        = $this->getMock(Composer::class);
+        $this->eventDispatcher = $this->getMockBuilder(EventDispatcher::class)->disableOriginalConstructor()->getMock();
+
+        $this->composer->expects(self::any())->method('getEventDispatcher')->willReturn($this->eventDispatcher);
+    }
+
     public function testActivate()
     {
-        $installer  = new Installer();
-        /* @var $composer Composer|\PHPUnit_Framework_MockObject_MockObject */
-        $composer   = $this->getMock(Composer::class);
-        $dispatcher = $this->getMockBuilder(EventDispatcher::class)->disableOriginalConstructor()->getMock();
-        /* @var $io IOInterface */
-        $io         = $this->getMock(IOInterface::class);
+        $this->eventDispatcher->expects(self::once())->method('addSubscriber')->with($this->installer);
 
-        $composer->expects(self::any())->method('getEventDispatcher')->willReturn($dispatcher);
-        $dispatcher->expects(self::once())->method('addSubscriber')->with($installer);
-
-        $installer->activate($composer, $io);
+        $this->installer->activate($this->composer, $this->io);
     }
 }
