944545a8ff56e4463da9f66d5caf8bb040186b21
Markus Staab <maggus.staab@googlemail.com>
2019-03-25 10:53:53 +0100

    added docs to Versions::VERSION
    the constant is public for technical reasons, therefore should be documented as `@internal`.
    
    Added a hint for the public api to use

--- src/PackageVersions/Installer.php
+++ src/PackageVersions/Installer.php
@@ -48,6 +48,12 @@ namespace PackageVersions;
 %s
 {
     public const ROOT_PACKAGE_NAME = '%s';
+    /**
+     * Array of all available composer packages.
+     * Dont read this array from your calling code, but use the \PackageVersions\Versions::getVersion() method instead.
+     *
+     * @internal
+     */
     public const VERSIONS          = %s;
 
     private function __construct()
