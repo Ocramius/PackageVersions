8bc0709f0a2c7dee9b14a5de3da035628ea530a2
Ken Stanley <kstanl27@uncc.edu>
2019-04-08 12:23:31 -0400

    Update unit tests

--- test/PackageVersionsTest/FallbackVersionsTest.php
+++ test/PackageVersionsTest/FallbackVersionsTest.php
@@ -23,27 +23,18 @@ final class FallbackVersionsTest extends TestCase
 {
     public function testWillFailWithoutValidComposerLockLocation() : void
     {
+        rename(__DIR__ . '/../../vendor/composer/installed.json', __DIR__ . '/../../vendor/composer/installed.json.backup');
         rename(__DIR__ . '/../../composer.lock', __DIR__ . '/../../composer.lock.backup');
 
-        try {
-            FallbackVersions::getVersion('phpunit/phpunit');
+        $this->expectException(\UnexpectedValueException::class);
+        $this->expectExceptionMessageRegExp(
+            '@PackageVersions could not locate the `vendor/composer/installed\.json` or your `composer\.lock` '
+            . 'location\. This is assumed to be in \[[^]]+?\]\. If you customized your composer vendor directory and ran composer '
+            . 'installation with --no-scripts or if you deployed without the required composer files, then you are on '
+            . 'your own, and we can\'t really help you\. Fix your shit and cut the tooling some slack\.@'
+        );
 
-            self::fail('An exception was supposed to be thrown');
-        } catch (UnexpectedValueException $lockFileNotFound) {
-            $srcDir = realpath(__DIR__ . '/../../src/PackageVersions');
-
-            self::assertSame(
-                'PackageVersions could not locate your `composer.lock` location. '
-                . 'This is assumed to be in '
-                . json_encode([$srcDir . '/../../../../../composer.lock', $srcDir . '/../../composer.lock'])
-                . '. If you customized your composer vendor directory and ran composer installation with --no-scripts, '
-                . 'then you are on your own, and we can\'t really help you. '
-                . 'Fix your shit and cut the tooling some slack.',
-                $lockFileNotFound->getMessage()
-            );
-        } finally {
-            rename(__DIR__ . '/../../composer.lock.backup', __DIR__ . '/../../composer.lock');
-        }
+        FallbackVersions::getVersion('phpunit/phpunit');
     }
 
     public function testValidVersions() : void
@@ -67,5 +58,16 @@ final class FallbackVersionsTest extends TestCase
         $this->expectException(OutOfBoundsException::class);
 
         FallbackVersions::getVersion(uniqid('', true) . '/' . uniqid('', true));
+    }
+
+    protected function tearDown()
+    {
+        if (file_exists(__DIR__ . '/../../vendor/composer/installed.json.backup')) {
+            rename(__DIR__ . '/../../vendor/composer/installed.json.backup', __DIR__ . '/../../vendor/composer/installed.json');
+        }
+
+        if (file_exists(__DIR__ . '/../../composer.lock.backup')) {
+            rename(__DIR__ . '/../../composer.lock.backup', __DIR__ . '/../../composer.lock');
+        }
     }
 }
