081a09087336661b4d5e12ca93f0cd3eea2b5875
Pavel Batanov <pavel@batanov.me>
2020-04-06 22:40:27 +0300

    Fix file order

--- src/PackageVersions/FallbackVersions.php
+++ src/PackageVersions/FallbackVersions.php
@@ -59,14 +59,14 @@ final class FallbackVersions
     private static function getPackageData() : array
     {
         $checkedPaths = [
+            // This package's composer.lock
+            __DIR__ . '/../../composer.lock',
             // The top-level project's ./vendor/composer/installed.json
             getcwd() . '/vendor/composer/installed.json',
             __DIR__ . '/../../../../composer/installed.json',
             // The top-level project's ./composer.lock
             getcwd() . '/composer.lock',
             __DIR__ . '/../../../../../composer.lock',
-            // This package's composer.lock
-            __DIR__ . '/../../composer.lock',
         ];
 
         $packageData = [];
